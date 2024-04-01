c185b485f743ae1477983f350cd3891934a664ba
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2024-04-01 07:17:47 +0200

    Implemented createPost, viewPost, and listPosts

---
+++ controller/post.controller.js
@@ -0,0 +1,81 @@
+import { resolve } from "path"
+import { validatePostData } from "../helpers/post.helpers"
+import postModel from "../model/post.model"
+
+const createPost = (post) => {
+    return new Promise((resolve, reject) => {
+        let errors = validatePostData(post)
+        if (Object.keys(errors).length > 0) {
+            reject({
+                code: 400,
+                msg: errors
+            })
+        } else {
+            postModel.createPost()
+            .then(data => resolve({
+                code: 200,
+                msg: data
+            }))
+            .catch(err => reject({
+                code: 500,
+                msg: err
+            }))
+        }
+    })
+}
+const viewPost = (postID) => {
+    return new Promise((resolve, reject) => {
+        postModel.findById(postID)
+        .then(data => {
+            if (data) {
+                resolve({
+                    code: 200,
+                    msg: data
+                })
+            } else {
+                reject({
+                    code: 404,
+                    msg: 'Post not found'
+                })
+            }
+        })
+        .catch(err => {
+            reject({
+                code: 500,
+                msg: err
+            })
+        })
+    })
+}
+const listPosts = (resultsPerPage, pageNumber) => {
+    return new Promise((resolve, reject) => {
+        if (isNaN(resultsPerPage) || isNaN(pageNumber)) {
+            reject({
+                code: 400,
+                msg: 'Invalid parameters'
+            })
+        } else {
+            postModel.find({
+                skip: resultsPerPage * (pageNumber - 1),
+                limit: resultsPerPage
+            })
+            .then(results => {
+                if (results.length > 0) {
+                    resolve({
+                        code: 200,
+                        msg: results
+                    })
+                } else {
+                    reject({
+                        code: 404,
+                        msg: "Posts not found"
+                    })
+                }
+            })
+        }
+    })
+}
+const updatePost = () => {}
+const deletePost = () => {}
+
+export { createPost, viewPost, listPosts, updatePost, deletePost }
