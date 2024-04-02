19962f180ed3c52bc94b02e8f7d95b9e5d6d9bf3
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2024-04-02 05:13:47 +0200

    Implemented createPost route

--- router/posts.router.js
+++ router/posts.router.js
@@ -1,16 +1,25 @@
 import { Router } from "express"
 
-import { listPosts } from "../controller/post.controller"
+import { createPost, listPosts } from "../controller/post.controller"
 
 const router = Router()
 
-router.get("/", (request,response) => {
-    listPosts(request.body.resultsPerPage, request.body.pageNumber)
-    .then(result => {
-        let responseCode = result.code
-        let resultData = result.msg
-        response.status(responseCode).json({ msg: resultData })
+router.route("/")
+    .get((request,response) => {
+        listPosts(request.body.resultsPerPage, request.body.pageNumber)
+        .then(result => {
+            let responseCode = result.code
+            let resultData = result.msg
+            response.status(responseCode).json({ msg: resultData })
+        })
     })
-})
+    .post((request, response) => {
+        createPost(request.body.post)
+        .then(result => {
+            let responseCode = result.code
+            let resultData = result.msg
+            response.status(responseCode).json({ msg: resultData })
+        })
+    })
 
 export default router
