402d9e34dd193fce1cac4203f0a80b0ea2f97799
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2024-04-04 01:37:10 +0200

    Refactored validatePostData for style consistency

--- helpers/post.helpers.js
+++ helpers/post.helpers.js
@@ -1,8 +1,9 @@
 const validatePostData = (data) => {
-    let validationErrors = {}
-    if (!data.title) validationErrors.title = 'Title is required'
-    if (!data.postText) validationErrors.postText = 'Post text is required'
-    return validationErrors
+    let errors = {}
+    !data.title || data.title == ""? errors.title = 'Title is required' : null
+    !data.postText || data.postText == ""? errors.postText = 'Post text is required' : null
+    !data.authorID || data.authorID == "" ? errors.authorID = 'Author ID is required' : null
+    return errors
 }
 
 export { validatePostData }
